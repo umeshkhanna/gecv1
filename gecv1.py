a4da52276111a2430e05ccc7c1e8126889bde85e
umesh <umesh@MacBook-Pro.domain.name>
2024-11-27 10:07:31 +0530

    new approach

--- gecv1.py
+++ gecv1.py
@@ -1,34 +1,12 @@
-import pandas as pd
-import datasets
-from datasets import load_dataset
-from tqdm import tqdm
-import argparse
-import glob
-import os
-import json
-import time
-import logging
-import random
-import re
-from itertools import chain
-from string import punctuation
-import nltk
-nltk.download('punkt')
-from nltk.tokenize import sent_tokenize
-import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+import pandas as pd
 from transformers import (
-    AdamW,
-    T5ForConditionalGeneration,
     T5Tokenizer,
-    get_linear_schedule_with_warmup,
-    Seq2SeqTrainingArguments, 
-    Seq2SeqTrainer, 
-    DataCollatorForSeq2Seq
+    T5ForConditionalGeneration,
+    TrainingArguments,
+    Trainer
 )
-from sklearn.model_selection import train_test_split
-
+from datasets import load_dataset
 import logging
 logging.basicConfig(
 filename="step.log",
@@ -40,35 +18,26 @@ datefmt="%Y-%m-%d %H:%M",
 )
 logging.warning("import done")
 
-def set_seed(seed):
-  random.seed(seed)
-  np.random.seed(seed)
-  torch.manual_seed(seed)
-
-set_seed(42)
-
-pd.set_option('display.max_colwidth', None)
-df = pd.read_csv('c4_200m_550k.csv')
-df.shape
-df.head()
-
+df = pd.read_csv('/content/Cleaned_Lang8.csv')
+# df.shape
+df.rename(columns={'0': 'Input', '1': 'Output'}, inplace=True)
 logging.warning("csv read successfully")
 
-model_name = 't5-base'
-tokenizer = T5Tokenizer.from_pretrained(model_name, padding='max_length', truncation=True)
-tokenizer.max_model_length = 512
-model = T5ForConditionalGeneration.from_pretrained(model_name)
-
-logging.warning("model instantiated")
+from sklearn.model_selection import train_test_split
+train_df, test_df = train_test_split(df, test_size=0.10, shuffle=True)
+train_df.shape, test_df.shape
 
 def calc_token_len(example):
     return len(tokenizer(example).input_ids)
 
-train_df, test_df = train_test_split(df, test_size=0.10, shuffle=True)
-train_df.shape, test_df.shape
+model_name = 't5-base'
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
 
-test_df['input_token_len'] = test_df['input'].apply(calc_token_len)
-test_df.head()
+logging.warning("model instantiated")
+
+test_df['input_token_len'] = test_df['Input'].apply(calc_token_len)
+# test_df.head()
 test_df['input_token_len'].describe()
 
 from datasets import Dataset
@@ -89,16 +58,16 @@ class GrammarDataset(Dataset):
 
 
     def tokenize_data(self, example):
-        input_, target_ = example['input'], example['output']
+        input_, target_ = example['Input'], example['Output']
 
         # tokenize inputs
-        tokenized_inputs = tokenizer(input_, pad_to_max_length=self.pad_to_max_length,
+        tokenized_inputs = tokenizer(input_, padding='max_length',
                                             max_length=self.max_len,
-                                            return_attention_mask=True)
+                                            return_attention_mask=True, truncation=True)
 
-        tokenized_targets = tokenizer(target_, pad_to_max_length=self.pad_to_max_length,
+        tokenized_targets = tokenizer(target_, padding='max_length',
                                             max_length=self.max_len,
-                                            return_attention_mask=True)
+                                            return_attention_mask=True, truncation=True)
 
         inputs={"input_ids": tokenized_inputs['input_ids'],
             "attention_mask": tokenized_inputs['attention_mask'],
@@ -117,59 +86,37 @@ class GrammarDataset(Dataset):
 
         return inputs
 
-dataset = GrammarDataset(test_dataset, tokenizer, True)
-data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, padding='longest', return_tensors='pt')
 
-# defining training related arguments
-batch_size = 16
-args = Seq2SeqTrainingArguments(output_dir="/notebooks/weights",
-                        evaluation_strategy="steps",
-                        per_device_train_batch_size=batch_size,
-                        per_device_eval_batch_size=batch_size,
-                        learning_rate=2e-5,
-                        num_train_epochs=1,
-                        weight_decay=0.01,
-                        save_total_limit=2,
-                        predict_with_generate=True,
-                        fp16 = True,
-                        gradient_accumulation_steps = 6,
-                        eval_steps = 500,
-                        save_steps = 500,
-                        load_best_model_at_end=True,
-                        report_to="none")
+BATCH_SIZE = 16
+MAX_LENGTH = 256
+EPOCHS = 1
+NUM_WORKERS = 2
+OUT_DIR = 'results_t5_base'
 
-import evaluate
-import nltk
-nltk.download('punkt')
-import numpy as np
+# Define the training arguments
+training_args = TrainingArguments(
+    output_dir=OUT_DIR,
+    num_train_epochs=EPOCHS,
+    per_device_train_batch_size=BATCH_SIZE,
+    per_device_eval_batch_size=BATCH_SIZE*2,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir=OUT_DIR,
+    eval_strategy='steps',
+    save_steps=500,
+    eval_steps=500,
+    load_best_model_at_end=True,
+    save_total_limit=2,
+    report_to='tensorboard'
+)
 
-# Setup evaluation
-nltk.download("punkt", quiet=True)
-metric = evaluate.load("rouge")
-
-def compute_metrics(eval_preds):
-    preds, labels = eval_preds
-
-    # decode preds and labels
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    # rougeLSum expects newline after each sentence
-    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
-    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
-
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
-    return result
-
-# defining trainer using 🤗
-trainer = Seq2SeqTrainer(model=model,
-                args=args,
-                train_dataset= GrammarDataset(train_dataset, tokenizer),
-                eval_dataset=GrammarDataset(test_dataset, tokenizer),
-                tokenizer=tokenizer,
-                data_collator=data_collator,
-                compute_metrics=compute_metrics)
+# Create the Trainer instance
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=GrammarDataset(train_dataset, tokenizer),
+    eval_dataset=GrammarDataset(test_dataset, tokenizer),
+)
 
 logging.warning("training started")
 
@@ -177,22 +124,6 @@ trainer.train()
 
 logging.warning("training ended")
 
-trainer.save_model('t5_gec_model_v1')
+trainer.save_model('t5_gec_model')
 
 logging.warning("model saved")
-
-import torch
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-model_name = 't5_gec_model_v1'
-torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name).to(torch_device)
-
-def correct_grammar(input_text,num_return_sequences):
-  batch = tokenizer([input_text],truncation=True,padding='max_length',max_length=64, return_tensors="pt").to(torch_device)
-  translated = model.generate(**batch,max_length=64,num_beams=4, num_return_sequences=num_return_sequences, temperature=1.5)
-  tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-  return tgt_text
-
-text = 'He are moving here.'
-print(correct_grammar(text, num_return_sequences=2))
